d63c8cbded5bfb01efb6876abdc4de4ab70d4581
harveyywang <harveyywang@tencent.com>
2022-02-08 13:13:40 +0800

    add: 第二章-序列构成的数据

---
+++ chapter_2-arraysOfSequences/sample_2.1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     sample_2.1
+   Description :
+   date：          2022/2/8
+-------------------------------------------------
+"""
+
+"""
+序列的分类
+1.按照存放的是对象的引用还是值
+    容器序列：list
+            tuple 
+            collections.deque 
+            
+    扁平序列：
+            str 
+            bytes
+            bytearray
+            memoryview
+            array.array
+2.按照能否被修改
+    可变序列(MutableSequence)：
+            list 
+            bytearray
+            array.array
+            collections.deque 
+            memoryview
+    不可变序列(Sequence)：
+            tuple
+            str 
+            bytes
+"""
+
+
+
